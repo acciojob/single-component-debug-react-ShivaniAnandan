43ef99ab4109f3e38c9ca82327d4f815434cde25
Shivani Anandan <shivanianandhan28@gmail.com>
2023-07-24 22:06:28 +0530

    final

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-
+import './commands'
+   Cypress.on('uncaught:exception', (err, runnable) => {
+   // returning false here prevents Cypress from
+ // failing the test
+   return false
+   })
 
 
 
