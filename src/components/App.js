65de0b0f9a260a769372aeb9ab27e8e26bb4015d
Shivani Anandan <shivanianandhan28@gmail.com>
2023-07-25 21:48:14 +0530

    3

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ const App = () => {
 
 return(
     <div>
-    <p id="main">Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+    <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
     </div>
 )
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import './commands'
-   Cypress.on('uncaught:exception', (err, runnable) => {
-   // returning false here prevents Cypress from
- // failing the test
-   return false
-   })
 
 
 
 
 
-ReactDOM.render(<App/>,document.getElementById("root"));
+
+ReactDOM.render(<App />, document.getElementById("root"));
